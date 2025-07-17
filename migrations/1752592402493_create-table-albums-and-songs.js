5fecb6e15d3d45523afd6f5dbefefe8d2f4e3873
Fachmi Aditama <fachmi8adtm@gmail.com>
2025-07-17 09:27:59 +0700

    Sebelum uninstall dicoding eslint dan install air bnb base

---
+++ migrations/1752592402493_create-table-albums-and-songs.js
@@ -0,0 +1,66 @@
+/* eslint-disable camelcase */
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+// export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+  });
+
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('albums');
+  pgm.dropTable('songs');
+};
---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+const mapDBSongToModel = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId : album_id
+});
+
+
+
+module.exports = { mapDBSongToModel };
