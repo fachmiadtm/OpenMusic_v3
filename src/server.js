c458a1334310cbede1ce169e9ed894e904bd0a10
Fachmi Aditama <fachmi8adtm@gmail.com>
2025-07-18 16:17:28 +0700

    Memperbaiki onPreResponse, dan maping

--- src/server.js
+++ src/server.js
@@ -41,25 +41,30 @@ const init = async () => {
   server.ext('onPreResponse', (request, h) => {
     const { response } = request;
 
-    if (response instanceof ClientError) {
+    if (response instanceof Error) {
+      
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
       const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
       });
-      newResponse.code(response.statusCode);
+      newResponse.code(500);
       return newResponse;
     }
 
-    if (!response.isServer) {
-      return h.continue;
-    }
-
-    const newResponse = h.response({
-      status: 'error',
-      message: 'terjadi kegagalan pada server kami',
-    });
-    newResponse.code(500);
-    return newResponse;
+    return h.continue;
   });
 
   await server.start();
